bea03c9971dc57f9840b80f54e4bf2315e7d72f1
zming <517046497@qq.com>
2017-11-16 23:46:45 +0800

    more beautiful, lol  i can't stop laughter

--- blog/templatetags/article_filter.py
+++ blog/templatetags/article_filter.py
@@ -8,6 +8,5 @@ def index_context(value):
     start = value.find('</', 100)
     end = value.find('>', start)
     end = 200 if end == -1 else end
-    res_context = value[:end+1] + "<br/> ......"
-    print(res_context)
+    res_context = value[:end+1] + "<br/>    &bull;  &bull;  &bull;"
     return res_context

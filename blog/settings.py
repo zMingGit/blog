b693a48cedf03e3d49d1a0360997c3c1f0531a01
root <517046497@qq.com>
2019-12-15 16:20:39 +0000

    add local_settings

--- blog/settings.py
+++ blog/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -145,3 +145,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissions'
     ]
 }
+
+try:
+    from local_settings import *  # noqa
+except ImportError:
+    pass

6c1d45485224d7a11a9e55112b3c472991e59965
zming <email@zming.info>
2020-01-06 00:58:29 +0800

    hide email account

--- blog/comment/tasks.py
+++ blog/comment/tasks.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 from blog.celery import app
 from django.core.mail import send_mail
+from blog.settings import SENDER_MAIL, RECEIVER_MAIL
 
 
 @app.task
@@ -9,7 +10,7 @@ def comment_noti(nickname, article_title):
     send_mail(
         '评论通知',
         '%s 给你的博文 %s 留了言',
-        '2585994482@qq.com',
-        ['email@zming.info'],
+        SENDER_MAIL,
+        [RECEIVER_MAIL],
         fail_silently=False,
     )
